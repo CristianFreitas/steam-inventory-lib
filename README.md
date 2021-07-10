074bf10ea2df1fb918b8c1b5d8f17e18acf6f7a8
Cristian Freitas <cristian.freitas@pontaltelecom.com.br>
2021-07-10 18:58:52 -0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# steam-inventory-lib
+# Project Title
+
+Steam inventory
+
+## Description
+
+View steam inventory
+
+## Getting Started
+
+### Dependencies
+
+* axios
+
+### Installing
+
+* npm install
+
+### Executing program
+
+* node index.js
---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios').default;
+
+class Inventory {
+
+    steam = {};
+
+    constructor(steam) {
+        this.steam = steam;
+    }
+    
+    async get () {
+        try {
+            const { data } = await axios.get(`http://steamcommunity.com/inventory/${this.steam.userid}/${this.steam.appid}/${this.steam.contextid}?l=${this.steam.language}&count=${this.steam.count}`);
+            return data;
+        } catch (error) {
+            console.log(error)    
+        }
+    }
+}
+
+// example
+const inventory = new Inventory({
+    userid: '76561198086790407', // id user
+    appid: 730, //steam id csgo
+    contextid: 2, // inventory
+    language: 'english', // language items
+    count: '5000' // max quantity items
+})
+inventory.get();
